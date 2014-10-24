e261f4d70ec19afc506d7297a96088751a82cf65
EricRooney <EricRooney@m-ize.com>
2014-10-24 15:17:32 -0400

    bin folder

--- gen/com/mize/test/BuildConfig.java
+++ gen/com/mize/test/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.servicelive.android.servicelivepro.test;
+package com.mize.test;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- gen/com/mize/test/R.java
+++ gen/com/mize/test/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.servicelive.android.servicelivepro.test;
+package com.mize.test;
 
 public final class R {
     public static final class attr {
