24b885cd4c4313ad88a8ed7c056d799468f1acec
EricRooney <EricRooney@m-ize.com>
2014-10-24 15:06:36 -0400

    initial commit

---
+++ gen/com/servicelive/android/servicelivepro/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.servicelive.android.servicelivepro.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
